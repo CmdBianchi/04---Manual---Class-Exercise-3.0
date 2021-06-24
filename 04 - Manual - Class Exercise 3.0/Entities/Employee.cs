bd79249d3e65182bf056c656bebd006a078bd16f
marcus bianchi <marcusbianchi92@hotmail.com>
2021-06-24 18:30:43 -0300

    Adicionar arquivos de projeto.

---
+++ 04 - Manual - Class Exercise 3.0/Entities/Employee.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+namespace Entities {
+    class Employee {
+        public string Name { get; set; }
+        public double Income { get; set; }
+        public double Tax { get; set; }
+
+        public Employee() { }
+        public Employee(string name, double income, double tax) {
+            Name = name;
+            Income = income;
+            Tax = tax;
+        }
+
+        public double NetSalary() {
+            return Income - Tax;
+        }
+        public void IncreaseSalay(double percentage) {
+            Income = (Income) * ((100 + percentage) / 100);                        
+        }
+
+        public override string ToString() {
+            return Name + ", $" + (Income - Tax);
+
+        }
+    }
+}
---
+++ 04 - Manual - Class Exercise 3.0/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Entities;
+namespace _04___Manual___Class_Exercise_3._0 {
+    class Program {
+        static void Main(string[] args) {
+            Employee employee = new Employee();
+            Console.Write("Nome do funcionário: "); employee.Name = Console.ReadLine();
+            Console.Write("Salário bruto: "); employee.Income = double.Parse(Console.ReadLine());
+            Console.Write("Imposto: "); employee.Tax = double.Parse(Console.ReadLine());
+            Console.WriteLine(employee.NetSalary());
+            Console.WriteLine("Digite a porcentagem para aumentar o salário: "); double percentage = double.Parse(Console.ReadLine());
+            employee.IncreaseSalay(percentage);
+            Console.WriteLine("");
+            Console.WriteLine(employee);
+        }
+    }
+}
